7a21e638040ef9b31d8ab4489ffca9d8fb4c5a3d
Xuedong Shang <shang.xuedong@yahoo.fr>
2018-12-27 19:27:33 +0100

    Writing a new SHA for infinitely-many armed problem

--- src/fixed_budget/seq_halv.jl
+++ src/fixed_budget/seq_halv.jl
@@ -108,3 +108,58 @@ function seq_halving_ref(mu::Array, budget::Integer, dist::String, rec::Function
 
 	return(recommendation, N, means, recommendations)
 end
+
+
+# Sequential Halving for Infinitely-Many Armed Bandits
+function seq_halving_infinite(reservoir, K::Integer, budget::Integer, dist::String, rec::Function = eba)
+	K = length(mu)
+	rounds = ceil(log2(K))
+
+	# Initialization
+	arms = [i for i in 1:K]
+	N = Int.(zeros(1, K))
+	S = zeros(1, K)
+	rewards = [[] for i in 1:K]
+	s_r = K
+	recommendations = zeros(1, budget)
+
+	for a in 1:K
+		N[a] = 1
+		S[a] = sample_arm(mu[a], dist)
+	end
+
+	# Elimination loop
+	j = 0
+	for r in 1:rounds
+		t_r = tr(budget, K, r)
+		for i in 1:t_r
+			for a in arms
+				new_sample = sample_arm(mu[a], dist)
+				append!(rewards[a], new_sample)
+				S[a] += new_sample
+				N[a] += 1
+				j += 1
+				if j > budget
+					continue
+				else
+					recommendations[j] = arms[rec(N[arms], S[arms])]
+				end
+			end
+		end
+		for i in 1:(s_r-ceil(s_r/2))
+			_, minindx = findmin([mean(rewards[a]) for a in arms])
+			deleteat!(arms, minindx)
+		end
+		s_r = ceil(s_r/2)
+	end
+
+	for t in (j+1):budget
+		recommendations[t] = rand(arms)
+	end
+
+	means = [mean(rewards[i]) for i in 1:K]
+	recommendation = rand(arms)
+	recommendations = Int.(recommendations)
+
+	return(recommendation, N, means, recommendations)
+end
---
+++ test/test_infinite.jl
@@ -0,0 +1,55 @@
+using PyPlot
+using ProgressMeter
+using Distributed
+
+addprocs(3)
+if Sys.KERNEL == :Darwin
+	@everywhere include("/Users/xuedong/Programming/PhD/BestArm.jl/src/BestArm.jl")
+elseif Sys.KERNEL == :Linux
+	@everywhere include("/home/xuedong/Documents/xuedong/phd/work/code/BestArm.jl/src/BestArm.jl")
+end
+# @everywhere using BestArm
+@everywhere using DistributedArrays
+
+# Problem setting
+dist = "Bernoulli"
+mu = [0.4, 0.5, 0.35, 0.3]
+budget = 1000
+mcmc = 10
+
+policies = [BestArm.seq_halving_ref, BestArm.ttts]
+policy_names = ["Sequential Halving", "Top-Two Thompson Sampling"]
+# policies = [BestArm.uniform, BestArm.succ_reject, BestArm.ugape_b, BestArm.seq_halving_ref, BestArm.ttts, BestArm.ts, BestArm.at_lucb]
+# policy_names = ["Uniform Sampling", "Successive Reject", "UGapEB", "Sequential Halving with Refresh", "Top-Two Thompson Sampling", "Thompson Sampling", "AT-LUCB"]
+lp = length(policies)
+
+
+# Options
+VERBOSE = true
+
+
+# Tests
+fig = figure()
+X = 1:budget
+for imeth in 1:lp
+	policy = policies[imeth]
+	regrets = zeros(1, budget)
+	if policy_names[imeth] == "Top-Two Thompson Sampling"
+		regrets_array = @DArray [BestArm.parallel_ttts(mu, budget, dist) for i = 1:mcmc]
+		for i in 1:mcmc
+			regrets += regrets_array[i]
+		end
+	else
+		@showprogress 1 string("Computing ", policy_names[imeth], "...") for k in 1:mcmc
+			_, _, _, recs = policy(mu, budget, dist)
+			regrets_current = BestArm.compute_regrets(mu, recs, budget)
+			regrets += regrets_current
+		end
+	end
+	plot(X, reshape(regrets/mcmc, budget, 1), label = policy_names[imeth])
+end
+
+xlabel("Allocation budget")
+ylabel("Expectation of the simple regret")
+grid("on")
+legend(loc=1)
