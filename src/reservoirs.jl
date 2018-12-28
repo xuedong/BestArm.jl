cf362b678ebc35c8f99b45f8e152eec3056844cb
Xuedong Shang <shang.xuedong@yahoo.fr>
2018-12-28 17:52:14 +0100

    Create a file for dealing with resevoirs

---
+++ src/reservoirs.jl
@@ -0,0 +1,13 @@
+function sample(reservoir::String, mu::Float64, sigma::Float64 = 1.0)
+	if dist == "Bernoulli"
+		return (rand() < mu)
+	elseif dist == "Beta"
+		return rand(Beta(mu, sigma))
+	elseif dist == "Poisson"
+		return rand(Poisson(mu))
+	elseif dist == "Exponential"
+		return - mu * log(rand())
+	elseif dist == "Gaussian"
+		return mu + sigma * randn()
+	end
+end
