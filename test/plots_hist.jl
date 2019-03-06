9d56a9d24b52f413b6c8620b2f6a526843cffca6
Xuedong Shang <shang.xuedong@yahoo.fr>
2019-03-06 20:08:17 +0100

    Finish the barplot script

--- test/plots_hist.jl
+++ test/plots_hist.jl
@@ -1,7 +1,7 @@
 using PyPlot
 # using ConfParser
 using HDF5
-using Seaborn
+# using Seaborn
 
 reservoir = "Beta"
 alphas = [1.0, 2.0, 3.0, 1.0]
@@ -11,15 +11,16 @@ budget = 64
 
 for iparam in 1:4
 	fig = figure()
-	Seaborn.set(style="darkgrid")
+	# Seaborn.set(style="darkgrid")
 
 	# running tests
-	nbins = budget + 1
+	x = [0:1:budget;]
 	num_arms = h5open(string("/home/xuedong/Documents/xuedong/phd/work/code/BestArm.jl/misc/log/infinite/", reservoir, "(", alphas[iparam], ",", betas[iparam], ")", "_", "dttts", "_N.h5"), "r") do file
     	read(file, "dttts")
 	end
+	num_arms = reshape(num_arms', (budget+1,))
 
-	h = plt[:hist](num_arms, nbins) # Histogram
+	bar(x, num_arms, color="#0f87bf", align="center", alpha=0.4) # Histogram
 
 	xlabel("Number of arm plays")
 	ylabel("Expectation of number of arms")
