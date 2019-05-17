b01ca125572018509d545f5d2ff092da8fe9e863
xuedong <shang.xuedong@yahoo.fr>
2019-05-17 02:36:27 +0200

    Upload some results

--- test/plots_shift.jl
+++ test/plots_shift.jl
@@ -10,15 +10,15 @@ shifts = [0.2, 0.4, 0.6, 0.8, 1.0]
 budget = 160
 mcmc = 1000
 
-for _ in 1:1
+for iparam in 1:1
         pulls = zeros(1, 0)
-        for i in 1:lenght(shifts)
+        for i in 1:length(shifts)
                 if Sys.KERNEL == :Linux
-                        arms = h5open(string("/home/xuedong/Documents/xuedong/phd/work/code/BestArm.jl/misc/log/shift/", reservoir, "(", alphas[iparam], ",", betas[iparam], ")", "_", shifts[i], ".h5"), "r") do file
+                        arms = h5open(string("/home/xuedong/Documents/xuedong/phd/work/code/BestArm.jl/misc/log/shift/", reservoir, "(", alphas[iparam], ",", betas[iparam], ")", "_", "dttts_", shifts[i], ".h5"), "r") do file
                                 read(file, "dttts")
                         end
                 elseif Sys.KERNEL == :Darwin
-                        arms = h5open(string("/Users/xuedong/Programming/PhD/BestArm.jl/misc/log/shift/", reservoir, "(", alphas[iparam], ",", betas[iparam], ")", "_", shifts[i], ".h5"), "r") do file
+                        arms = h5open(string("/Users/xuedong/Programming/PhD/BestArm.jl/misc/log/shift/", reservoir, "(", alphas[iparam], ",", betas[iparam], ")", "_", "dttts_", shifts[i], ".h5"), "r") do file
                                 read(file, "dttts")
                         end
                 end
@@ -27,7 +27,7 @@ for _ in 1:1
                 pulls = hcat(pulls, arms)
         end
 
-        group = repeat(["shift=0.2", "shift=0.4", "shift=0.6", "shift=0.8", "no shift"], inner = 10)
+        group = repeat(["shift=0.2", "shift=0.4", "shift=0.6", "shift=0.8", "shift=1.0"], inner = 10)
         # std = [2, 3, 4, 1, 2, 3, 5, 2, 3, 3]
         xtick = repeat(["i=1", "i=2", "i=3", "i=4", "i=5", "i=6", "i=7", "i=8", "i=9", "i>=10"], outer = 5)
 
