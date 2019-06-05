38c68e81a8c05d062f749b463f4a803df035d7ca
Xuedong Shang <shang.xuedong@yahoo.fr>
2019-06-05 18:11:26 +0200

    Create a new visualization script for reservoirs

---
+++ visualizations/reservoirs.py
@@ -0,0 +1,92 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+from labellines import labelLines
+
+
+# def stick_legend(axis=None):
+#
+#     if axis is None:
+#         axis = plt.gca()
+#
+#     n = 32
+#     nlines = len(axis.lines)
+#     print(nlines)
+#
+#     xmin, xmax = axis.get_xlim()
+#     ymin, ymax = axis.get_ylim()
+#
+#     # the 'point of presence' matrix
+#     pop = np.zeros((nlines, n, n), dtype=np.float)
+#
+#     for l in range(nlines):
+#         # get xy data and scale it to the NxN squares
+#         xy = axis.lines[l].get_xydata()
+#         xy = (xy - [xmin, ymin]) / ([xmax-xmin, ymax-ymin]) * n
+#         xy = xy.astype(np.int32)
+#         # mask stuff outside plot
+#         mask = (xy[:, 0] >= 0) & (xy[:, 0] < n) & (xy[:, 1] >= 0) & (xy[:, 1] < n)
+#         xy = xy[mask]
+#         # add to pop
+#         for p in xy:
+#             pop[l][tuple(p)] = 1.0
+#
+#     # find whitespace, nice place for labels
+#     ws = 1.0 - (np.sum(pop, axis=0) > 0) * 1.0
+#     # don't use the borders
+#     ws[:, 0] = 0
+#     ws[:, n-1] = 0
+#     ws[0, :] = 0
+#     ws[n-1, :] = 0
+#
+#     # blur the pop's
+#     for l in range(nlines):
+#         pop[l] = ndimage.gaussian_filter(pop[l], sigma=n/5)
+#
+#     for l in range(nlines):
+#         # positive weights for current line, negative weight for others....
+#         w = -0.3 * np.ones(nlines, dtype=np.float)
+#         w[l] = 0.5
+#
+#         # calculate a field
+#         p = ws + np.sum(w[:, np.newaxis, np.newaxis] * pop, axis=0)
+#         plt.figure()
+#         plt.imshow(p, interpolation='nearest')
+#         plt.title(axis.lines[l].get_label())
+#
+#         pos = np.argmax(p)  # note, argmax flattens the array first
+#         best_x, best_y = pos / n, pos % n
+#         x = xmin + (xmax-xmin) * best_x / n
+#         y = ymin + (ymax-ymin) * best_y / n
+#
+#         axis.text(x, y, axis.lines[l].get_label(),
+#                   horizontalalignment='center',
+#                   verticalalignment='center')
+
+
+def nu_0(x):
+    """
+    A wavy function as an example of reservoir.
+
+    :param x: some real number
+    :return: y
+    """
+    return 5*np.sin(x) + 2*x - x**2 + .3*x**3 - .2*(x-15)**4 - 10*x**2 * np.cos(x/3+12)**3 + .5*(x-12)**4
+
+
+if __name__ == '__main__':
+    x_values = np.linspace(10.0, 19.0, 100000)
+    y_values = np.array([nu_0(x) for x in x_values])
+
+    plt.close('all')
+
+    sns.set()
+
+    plt.plot(x_values, y_values, label=r'$\nu_0$')
+    labelLines(plt.gca().get_lines())
+    plt.xticks(np.arange(10, 20), ('0', '', '', '', '', '', '', '', '', r'$\mu^\star$'))
+    plt.yticks([])
+    plt.xlabel('means')
+
+    plt.savefig('../misc/figs/reservoir.pdf')
