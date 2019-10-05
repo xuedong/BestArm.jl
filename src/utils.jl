1c33e72f4938386e97379255f5c2eeb9c6777419
Xuedong Shang <shang.xuedong@yahoo.fr>
2019-10-05 14:49:00 +0200

    Add a helper function

--- src/utils.jl
+++ src/utils.jl
@@ -50,6 +50,17 @@ function mpa(N, S)
 end
 
 
+function randmax(vector, rank=1)
+   # returns an integer, not a CartesianIndex
+   vector = vec(vector)
+   Sorted = sort(vector,rev=true)
+   m = Sorted[rank]
+   Ind = findall(x->x==m,vector)
+   index = Ind[floor(Int,length(Ind)*rand())+1]
+   return index
+end
+
+
 # Helper functions for UCB-E
 function compute_ucbe(mean, a, s)
 	if s > 0
