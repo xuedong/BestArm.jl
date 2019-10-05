33de5e7da7fc48bcea5ed7d17bc41677050e4ed6
Xuedong Shang <shang.xuedong@yahoo.fr>
2019-10-05 14:58:27 +0200

    New version of D-Tracking

--- src/fixed_confidence/track_stop.jl
+++ src/fixed_confidence/track_stop.jl
@@ -1,62 +1,63 @@
-function track_stop(mu::Array, delta::Real, rate::Function, dist::String)
-    condition = true
-    K=length(mu)
-    N = zeros(1,K)
-    S = zeros(1,K)
-    # initialization
-    for a in 1:K
-        N[a]=1
-        S[a]=sample_arm(mu[a], dist)
-    end
-    t=K
-    Best=1
-    while (condition)
-        Mu=S./N
-        # Empirical best arm
-        IndMax=find(Mu.==maximum(Mu))
-        Best=IndMax[floor(Int,length(IndMax)*rand())+1]
-        I=1
-        if (length(IndMax)>1)
-            # if multiple maxima, draw one them at random
-            I = Best
-        else
-            # compute the stopping statistic
-            NB=N[Best]
-            SB=S[Best]
-            muB=SB/NB
-            MuMid=(SB+S)./(NB+N)
-            Index=collect(1:K)
-            splice!(Index,Best)
-            Score=minimum([NB*d(muB, MuMid[i], dist)+N[i]*d(Mu[i], MuMid[i], dist) for i in Index])
-            if (Score > rate(t,0,delta))
-                # stop
-                condition=false
-            elseif (t >10000000)
-                # stop and outputs (0,0)
-                condition=false
-                Best=0
-                print(N)
-                print(S)
-                N=zeros(1,K)
-            else
-                if (minimum(N) <= max(sqrt(t) - K/2,0))
-                    # forced exploration
-                    I=indmin(N)
-                else
-                    # continue and sample an arm
-                    val,Dist=OptimalWeights(Mu,1e-11)
-                    # choice of the arm
-                    I=indmax(Dist-N/t)
-                end
-            end
-        end
-        # draw the arm
-        t+=1
-        S[I]+=sample_arm(mu[I], dist)
-        N[I]+=1
-    end
-    recommendation=Best
-    return (recommendation,N)
+function d_tracking(mu::Array, delta::Real, rate::Function, dist::String)
+   	# Chernoff stopping + D-Tracking
+   	condition = true
+   	K = length(mu)
+   	N = zeros(1,K)
+   	S = zeros(1,K)
+   	# initialization
+   	for a in 1:K
+      	N[a] = 1
+      	S[a] = sample(mu[a])
+   	end
+   	t = K
+   	Best = 1
+   	while (condition)
+      	Mu = S./N
+      	# Empirical best arm
+      	IndMax = findall(x -> x==maximum(Mu),Mu)
+      	Best = IndMax[floor(Int, length(IndMax)*rand())+1]
+      	I = 1
+      	if (length(IndMax) > 1)
+		  	# if multiple maxima, draw one them at random
+		  	I = Best
+      	else
+		  	# compute the stopping statistic
+		  	NB = N[Best]
+         	SB = S[Best]
+         	muB = SB/NB
+         	MuMid = (SB.+S)./(NB.+N)
+         	Index = collect(1:K)
+         	deleteat!(Index, Best[2])
+         	Score = minimum([NB*d(muB, MuMid[i])+N[i]*d(Mu[i], MuMid[i]) for i in Index])
+         	if (Score > rate(t, delta))
+            	# stop
+            	condition = false
+         	elseif (t >10000000)
+            	# stop and outputs (0,0)
+            	condition = false
+            	Best = 0
+            	print(N)
+            	print(S)
+            	N = zeros(1,K)
+         	else
+            	if (minimum(N) <= max(sqrt(t) - K/2,0))
+               		# forced exploration
+               		I = argmin(N)
+            	else
+               	 	# continue and sample an arm
+	            	val, Dist = optimal_weights(Mu, dist, 1e-11)
+               		# choice of the arm
+               		I = argmax(Dist-N/t)
+            	end
+	      	end
+      	end
+      	# draw the arm
+      	t += 1
+      	S[I] += sample(mu[I])
+      	N[I] += 1
+   	end
+   	recommendation = Best[2]
+   	return (recommendation, N)
 end
 
 
