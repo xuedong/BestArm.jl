f202e6d2467dc53d919d3b077d8ea64f891e0ad8
Xuedong Shang <shang.xuedong@yahoo.fr>
2019-10-31 18:43:44 +0100

    Fix some docstring issue in BestArm.jl

--- src/BestArm.jl
+++ src/BestArm.jl
@@ -12,20 +12,17 @@ module BestArm
     export optimal_weights
     export gamma_beta
 
-    """anytime"""
     export at_lucb
     export ts
     export ttts, parallel_ttts, ttts_infinite, ttts_dynamic
     export ttps, parallel_ttps
 
-    """fixed-budget"""
     export seq_halving_ref, seq_halving_no_ref, seq_halving_infinite, hyperband
     export succ_reject
     export ucbe, ucbe_adaptive
     export ugape_b, ugape_b_adaptive
     export uniform
 
-    """fixed-confidence"""
     export best_challenger, best_challenger_bis, best_challenger_ts
     export kl_lucb
     export racing, kl_racing
@@ -37,10 +34,7 @@ module BestArm
     export ugape_c
     export uniform_c
 
-    """infinite bandit"""
     export siri
-
-    """linear bandit"""
 
     include("arms.jl")
     include("utils.jl")
