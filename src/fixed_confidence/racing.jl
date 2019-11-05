929dccf8c07e0b51409dea9ed6d10a2c0a6eb260
Xuedong Shang <shang.xuedong@yahoo.fr>
2019-11-05 19:36:09 +0100

    Update racing to julia 1.1

--- src/fixed_confidence/racing.jl
+++ src/fixed_confidence/racing.jl
@@ -1,92 +1,48 @@
-function chernoff_racing(mu::Array, delta::Real, rate::Function, dist::String)
+function racing(mu::Array, delta::Real, rate::Function, dist::String,
+	elimination::Symbol=:Chernoff)
     condition = true
-    K=length(mu)
-    N = zeros(1,K)
-    S = zeros(1,K)
-    # initialization
-    for a in 1:K
-        N[a]=1
-        S[a]=sample_arm(mu[a], dist)
-    end
-    round=1
-    t=K
-    Best=1
-    Remaining=collect(1:K)
-    while (length(Remaining)>1)
-        # Drawn all remaining arms
-        for a in Remaining
-            S[a]+=sample_arm(mu[a], dist)
-            N[a]+=1
-        end
-        round+=1
-        t+=length(Remaining)
-        # Check whether the worst should be removed
-        Mu=S./N
-        MuR=Mu[Remaining]
-        MuBest=maximum(MuR)
-        IndBest=find(MuR.==MuBest)[1]
-        IndBest=IndBest[floor(Int,rand()*length(IndBest))+1]
-        Best=Remaining[IndBest]
-        MuWorst=minimum(MuR)
-        IndWorst=find(MuR.==MuWorst)[1]
-        IndWorst=IndWorst[floor(Int,rand()*length(IndWorst))+1]
-        if (round*(d(MuBest, (MuBest+MuWorst)/2, dist)+d(MuWorst, (MuBest+MuWorst)/2, dist)) > rate(t, 0, delta))
-            # remove Worst arm
-            splice!(Remaining,IndWorst)
-        end
-        if (t>1000000)
-            Remaining=[]
-            Best=0
-            N=zeros(1,K)
-        end
-    end
-    recommendation=Best
-    return (recommendation,N)
-end
-
-
-function kl_racing(mu::Array, delta::Real, rate::Function, dist::String)
-    condition = true
-    K=length(mu)
-    N = zeros(1,K)
-    S = zeros(1,K)
-    # initialization
-    for a in 1:K
-        N[a]=1
-        S[a]=sample_arm(mu[a], dist)
-    end
-    round=1
-    t=K
-    Best=1
-    Remaining=collect(1:K)
-    while (length(Remaining)>1)
-        # Drawn all remaining arms
-        for a in Remaining
-            S[a]+=sample_arm(mu[a], dist)
-            N[a]+=1
-        end
-        round+=1
-        t+=length(Remaining)
-        # Check whether the worst should be removed
-        Mu=S./N
-        MuR=Mu[Remaining]
-        MuBest=maximum(MuR)
-        IndBest=find(MuR.==MuBest)[1]
-        Best=IndBest[floor(Int,rand()*length(IndBest))+1]
-        Best=Remaining[Best]
-        MuWorst=minimum(MuR)
-        IndWorst=find(MuR.==MuWorst)[1]
-        IndWorst=IndWorst[floor(Int,rand()*length(IndWorst))+1]
-        if (dlow(MuBest, rate(t,round,delta)/round, dist) > dup(MuWorst, rate(t,round,delta)/round, dist))
-            # remove Worst arm
-            splice!(Remaining,IndWorst)
-        end
-        if (t>1000000)
-            Remaining=[]
-            Best=0
-            N=zeros(1,K)
-        end
-    end
-    recommendation=Best
-    return (recommendation,N)
+  K=length(mu)
+  N = zeros(1,K)
+  S = zeros(1,K)
+  # initialization
+  for a in 1:K
+      N[a]=1
+      S[a]=sample(mu[a])
+  end
+  round=1
+  t=K
+  Remaining=collect(1:K)
+  while (length(Remaining)>1)
+      # Drawn all remaining arms
+      for a in Remaining
+	      S[a]+=sample(mu[a])
+	      N[a]+=1
+      end
+      round+=1
+      t+=length(Remaining)
+      # Check whether the worst should be removed
+      Mu=S./N
+      MuR=Mu[Remaining]
+      MuBest=maximum(MuR)
+      IndBest=randmax(MuR)
+      Best=Remaining[IndBest]
+      MuWorst=minimum(MuR)
+      IndWorst=randmax(-MuR)
+	  if elimination == "Chernoff"
+      	if (round*(d(MuBest, (MuBest+MuWorst)/2)+d(MuWorst,(MuBest+MuWorst)/2)) > rate(t,delta))
+         	# remove Worst arm
+         	deleteat!(Remaining,IndWorst)
+      	end
+	elseif elimination == "KL"
+		if (dlow(MuBest,rate(round,delta)/round) > dup(MuWorst,rate(round,delta)/round))
+         # remove Worst arm
+         deleteat!(Remaining,IndWorst)
+      end
+      if (t>1000000)
+	      Remaining=[0]
+         N=zeros(1,K)
+      end
+   end
+   recommendation=Remaining[1]
+   return (recommendation,N)
 end
