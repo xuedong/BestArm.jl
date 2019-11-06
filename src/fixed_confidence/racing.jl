c918a1f0a386bafac30d576c9e9609706076af6c
Xuedong Shang <shang.xuedong@yahoo.fr>
2019-11-06 19:11:29 +0100

    Debug racing

--- src/fixed_confidence/racing.jl
+++ src/fixed_confidence/racing.jl
@@ -1,48 +1,51 @@
 function racing(mu::Array, delta::Real, rate::Function, dist::String,
 	elimination::Symbol=:Chernoff)
     condition = true
-  K=length(mu)
-  N = zeros(1,K)
-  S = zeros(1,K)
-  # initialization
-  for a in 1:K
-      N[a]=1
-      S[a]=sample(mu[a])
-  end
-  round=1
-  t=K
-  Remaining=collect(1:K)
-  while (length(Remaining)>1)
-      # Drawn all remaining arms
-      for a in Remaining
-	      S[a]+=sample(mu[a])
-	      N[a]+=1
-      end
-      round+=1
-      t+=length(Remaining)
-      # Check whether the worst should be removed
-      Mu=S./N
-      MuR=Mu[Remaining]
-      MuBest=maximum(MuR)
-      IndBest=randmax(MuR)
-      Best=Remaining[IndBest]
-      MuWorst=minimum(MuR)
-      IndWorst=randmax(-MuR)
-	  if elimination == "Chernoff"
-      	if (round*(d(MuBest, (MuBest+MuWorst)/2)+d(MuWorst,(MuBest+MuWorst)/2)) > rate(t,delta))
-         	# remove Worst arm
-         	deleteat!(Remaining,IndWorst)
+  	K = length(mu)
+  	N = zeros(1,K)
+  	S = zeros(1,K)
+  	# initialization
+  	for a in 1:K
+      	N[a] = 1
+      	S[a] = sample_arm(mu[a], dist)
+  	end
+  	round=1
+  	t=K
+  	Remaining=collect(1:K)
+  	while (length(Remaining)>1)
+      	# Drawn all remaining arms
+      	for a in Remaining
+	      	S[a] += sample(mu[a], dist)
+	      	N[a] += 1
       	end
-	elseif elimination == "KL"
-		if (dlow(MuBest,rate(round,delta)/round) > dup(MuWorst,rate(round,delta)/round))
-         # remove Worst arm
-         deleteat!(Remaining,IndWorst)
-      end
-      if (t>1000000)
-	      Remaining=[0]
-         N=zeros(1,K)
-      end
-   end
-   recommendation=Remaining[1]
-   return (recommendation,N)
+      	round+=1
+      	t+=length(Remaining)
+      	# Check whether the worst should be removed
+      	Mu=S./N
+      	MuR=Mu[Remaining]
+      	MuBest=maximum(MuR)
+      	IndBest=randmax(MuR)
+      	Best=Remaining[IndBest]
+      	MuWorst=minimum(MuR)
+      	IndWorst=randmax(-MuR)
+	  	if elimination == "Chernoff"
+      		if (round*(d(MuBest, (MuBest+MuWorst)/2, dist)+d(MuWorst, (MuBest+MuWorst)/2), dist) > rate(t,delta))
+         		# remove Worst arm
+         		deleteat!(Remaining, IndWorst)
+      		end
+		elseif elimination == "KL"
+			kl_low = dlow(MuBest, rate(round, delta)/round, dist)
+			kl_up = dup(MuWorst, rate(round, delta)/round, dist)
+			if kl_low > kl_up
+         		# remove Worst arm
+         		deleteat!(Remaining, IndWorst)
+			end
+      	end
+      	if (t>1000000)
+	      	Remaining=[0]
+         	N=zeros(1,K)
+      	end
+   	end
+   	recommendation=Remaining[1]
+   	return (recommendation,N)
 end
