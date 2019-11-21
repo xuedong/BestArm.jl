75c5867bddadc234f05318e1ba01810e70be813f
Xuedong Shang <shang.xuedong@yahoo.fr>
2019-11-21 11:59:05 +0100

    Format code of linear test

--- test/test_linear.jl
+++ test/test_linear.jl
@@ -2,9 +2,9 @@ using HDF5
 using Distributed
 
 if Sys.KERNEL == :Darwin
-	@everywhere include("/Users/xuedong/Programming/PhD/BestArm.jl/src/BestArm.jl")
+    @everywhere include("/Users/xuedong/Programming/PhD/BestArm.jl/src/BestArm.jl")
 elseif Sys.KERNEL == :Linux
-	@everywhere include("/home/xuedong/Documents/xuedong/phd/work/code/BestArm.jl/src/BestArm.jl")
+    @everywhere include("/home/xuedong/Documents/xuedong/phd/work/code/BestArm.jl/src/BestArm.jl")
 end
 
 # DO YOU WANT TO SAVE RESULTS?
@@ -12,10 +12,10 @@ end
 typeExp = "NoSave"
 
 # TYPE OF DISTRIBUTION
-@everywhere distribution="Gaussian"
+@everywhere distribution = "Gaussian"
 
 # CHANGE NAME (save mode)
-fname="/home/xuedong/Downloads/t3c/results/xs"
+fname = "/home/xuedong/Downloads/t3c/results/xs"
 
 # BANDIT PROBLEM
 @everywhere c1 = [1, 0, 0, 0, 0]
@@ -41,7 +41,7 @@ N = 1
 
 # OPTIMAL SOLUTION
 @everywhere v, optWeights = BestArm.optimal_weights(mu, distribution)
-@everywhere gammaOpt=optWeights[best]
+@everywhere gammaOpt = optWeights[best]
 print("mu=$(mu)\n")
 print("Theoretical number of samples: $(v*log(1/delta))\n")
 print("Optimal weights: $(optWeights)\n\n")
@@ -52,94 +52,94 @@ print("Optimal weights: $(optWeights)\n\n")
 @everywhere namesPolicies = ["L-T3S"]
 
 # EXPLORATION RATES
-@everywhere explo(t, delta) = log((log(t)+1)/delta)
+@everywhere explo(t, delta) = log((log(t) + 1) / delta)
 
 lP = length(policies)
-rates = [explo for i in 1:lP]
+rates = [explo for i = 1:lP]
 
 
 # RUN EXPERIMENTS
 
-function MCexp(mu,delta,N)
-	for imeth=1:lP
-		Draws=zeros(N,K)
-		policy=policies[imeth]
-		rate=rates[imeth]
-		startTime=time()
-		Reco, Draws = @distributed ((x,y) -> (vcat(x[1], y[1]), vcat(x[2], y[2]))) for n in 1:N
-				rec, dra = policy(contexts, true_theta, delta, rate, distribution)
-				rec, dra
-		end
-		Error=collect([(r==best) ? 0 : 1 for r in Reco])
-		FracNT=sum([r==0 for r in Reco])/N
-		FracReco=zeros(K)
-		proportion = zeros(K)
-		for k in 1:K
-			FracReco[k]=sum([(r==k) ? 1 : 0 for r in Reco])/(N*(1-FracNT))
-		end
-		for n in 1:N
-			if (Reco[n]!=0)
-			    proportion += Draws[n,:]/sum(Draws[n,:])
-			end
-		end
-		proportion = proportion / (N*(1-FracNT))
-		print("Results for $(policy), average on $(N) runs\n")
-		print("proportion of runs that did not terminate: $(FracNT)\n")
-		print("average number of draws: $(sum(Draws)/(N*(1-FracNT)))\n")
-		print("average proportions of draws: $(proportion)\n")
-		print("proportion of errors: $(sum(Error)/(float(N*(1-FracNT))))\n")
-		print("proportion of recommendation made when termination: $(FracReco)\n")
-		print("elapsed time: $(time()-startTime)\n\n")
-	end
+function MCexp(mu, delta, N)
+    for imeth = 1:lP
+        Draws = zeros(N, K)
+        policy = policies[imeth]
+        rate = rates[imeth]
+        startTime = time()
+        Reco, Draws = @distributed ((x, y) -> (vcat(x[1], y[1]), vcat(x[2], y[2]))) for n = 1:N
+            rec, dra = policy(contexts, true_theta, delta, rate, distribution)
+            rec, dra
+        end
+        Error = collect([(r == best) ? 0 : 1 for r in Reco])
+        FracNT = sum([r == 0 for r in Reco]) / N
+        FracReco = zeros(K)
+        proportion = zeros(K)
+        for k = 1:K
+            FracReco[k] = sum([(r == k) ? 1 : 0 for r in Reco]) / (N * (1 - FracNT))
+        end
+        for n = 1:N
+            if (Reco[n] != 0)
+                proportion += Draws[n, :] / sum(Draws[n, :])
+            end
+        end
+        proportion = proportion / (N * (1 - FracNT))
+        print("Results for $(policy), average on $(N) runs\n")
+        print("proportion of runs that did not terminate: $(FracNT)\n")
+        print("average number of draws: $(sum(Draws)/(N*(1-FracNT)))\n")
+        print("average proportions of draws: $(proportion)\n")
+        print("proportion of errors: $(sum(Error)/(float(N*(1-FracNT))))\n")
+        print("proportion of recommendation made when termination: $(FracReco)\n")
+        print("elapsed time: $(time()-startTime)\n\n")
+    end
 end
 
 
-function SaveData(mu,delta,N)
-	K=length(mu)
-    for imeth=1:lP
-        Draws=zeros(N,K)
-        policy=policies[imeth]
-		beta=rates[imeth]
-        namePol=namesPolicies[imeth]
-        startTime=time()
-		Reco,Draws = @distributed ((x,y) -> (vcat(x[1],y[1]),vcat(x[2],y[2]))) for n in 1:N
-	        reco,draws = policy(mu,delta,beta)
-	        reco,draws
-	    end
-		Error=collect([(r==best) ? 0 : 1 for r in Reco])
-        FracNT=sum([r==0 for r in Reco])/N
-        FracReco=zeros(K)
-		proportion = zeros(K)
-        for k in 1:K
-            FracReco[k]=sum([(r==k) ? 1 : 0 for r in Reco])/(N*(1-FracNT))
-		end
-		for n in 1:N
-			if (Reco[n]!=0)
-			   proportion += Draws[n,:]/sum(Draws[n,:])
-			end
+function SaveData(mu, delta, N)
+    K = length(mu)
+    for imeth = 1:lP
+        Draws = zeros(N, K)
+        policy = policies[imeth]
+        beta = rates[imeth]
+        namePol = namesPolicies[imeth]
+        startTime = time()
+        Reco, Draws = @distributed ((x, y) -> (vcat(x[1], y[1]), vcat(x[2], y[2]))) for n = 1:N
+            reco, draws = policy(mu, delta, beta)
+            reco, draws
         end
-		proportion = proportion /(N*(1-FracNT))
+        Error = collect([(r == best) ? 0 : 1 for r in Reco])
+        FracNT = sum([r == 0 for r in Reco]) / N
+        FracReco = zeros(K)
+        proportion = zeros(K)
+        for k = 1:K
+            FracReco[k] = sum([(r == k) ? 1 : 0 for r in Reco]) / (N * (1 - FracNT))
+        end
+        for n = 1:N
+            if (Reco[n] != 0)
+                proportion += Draws[n, :] / sum(Draws[n, :])
+            end
+        end
+        proportion = proportion / (N * (1 - FracNT))
         print("Results for $(policy), average on $(N) runs\n")
-	    print("proportion of runs that did not terminate: $(FracNT)\n")
-	    print("average number of draws: $(sum(Draws)/(N*(1-FracNT)))\n")
-		print("average proportions of draws: $(proportion)\n")
-	    print("proportion of errors: $(sum(Error)/(float(N*(1-FracNT))))\n")
+        print("proportion of runs that did not terminate: $(FracNT)\n")
+        print("average number of draws: $(sum(Draws)/(N*(1-FracNT)))\n")
+        print("average proportions of draws: $(proportion)\n")
+        print("proportion of errors: $(sum(Error)/(float(N*(1-FracNT))))\n")
         print("proportion of recommendation made when termination: $(FracReco)\n")
         print("elapsed time: $(time()-startTime)\n")
-		print("one step time: $((time()-startTime)/((sum(Draws)/(N*(1-FracNT)))))\n\n")
-        name="$(fname)_$(namePol)_delta_$(delta)_N_$(N).h5"
-        h5write(name,"mu",mu)
-        h5write(name,"delta",delta)
-        h5write(name,"FracNT",collect(FracNT))
-        h5write(name,"FracReco",FracReco)
-        h5write(name,"Draws",Draws)
-        h5write(name,"Error",mean(Error))
-	end
+        print("one step time: $((time()-startTime)/((sum(Draws)/(N*(1-FracNT)))))\n\n")
+        name = "$(fname)_$(namePol)_delta_$(delta)_N_$(N).h5"
+        h5write(name, "mu", mu)
+        h5write(name, "delta", delta)
+        h5write(name, "FracNT", collect(FracNT))
+        h5write(name, "FracReco", FracReco)
+        h5write(name, "Draws", Draws)
+        h5write(name, "Error", mean(Error))
+    end
 end
 
 
-if (typeExp=="Save")
-   SaveData(mu,delta,N)
+if (typeExp == "Save")
+    SaveData(mu, delta, N)
 else
-   MCexp(mu,delta,N)
+    MCexp(mu, delta, N)
 end
