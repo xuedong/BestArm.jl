0fa7ce0335072c94c4e10cb92e892b86f8171801
Xuedong Shang <shang.xuedong@yahoo.fr>
2019-11-27 18:52:29 +0100

    Minor changes

--- test/test_weights.jl
+++ test/test_weights.jl
@@ -12,7 +12,7 @@ end
 # 		[0.5, 0.45, 0.425, 0.4, 0.375, 0.35, 0.325, 0.3, 0.275, 0.25, 0.225, 0.2, 0.175, 0.15, 0.125],
 # 		[0.5, 0.48, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37, 0.37],
 # 		[0.5, 0.45, 0.45, 0.45, 0.45, 0.45, 0.43, 0.43, 0.43, 0.43, 0.43, 0.43, 0.43, 0.43, 0.43, 0.43, 0.43, 0.43, 0.43, 0.43, 0.38, 0.38, 0.38, 0.38, 0.38, 0.38, 0.38, 0.38, 0.38, 0.38]]
-random_lengths = rand(3:10, 100)
+random_lengths = rand(3:6, 100)
 mus = [sort(rand(random_lengths[i]), rev=true) for i in 1:length(random_lengths)]
 dist = "Bernoulli"
 len = length(mus)
@@ -23,11 +23,13 @@ gammas = zeros(1, len)
 for i in 1:len
 	gamma = BestArm.gamma_beta(mus[i], dist, 0.5, 1e-11)
 	gammas[i] = gamma
+	println(mus[i])
 end
+println(gammas)
 
-io = open("gammas.txt", "w")
-for i in 1:length(gammas)
-	gamma = gammas[i]
-	write(io, "$gamma\n")
-end
-close(io)
+#io = open("gammas.txt", "w")
+#for i in 1:length(gammas)
+#	gamma = gammas[i]
+#	write(io, "$gamma\n")
+#end
+#close(io)
