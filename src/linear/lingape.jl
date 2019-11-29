59855b8dd78d04ac6a86de9841e6e2e96cce1e61
Xuedong Shang <shang.xuedong@yahoo.fr>
2019-11-29 17:44:12 +0100

    Implement LinGapE

--- src/linear/lingape.jl
+++ src/linear/lingape.jl
@@ -1,4 +1,13 @@
-function lingape(contexts::Array, theta::Array, delta::Real, rate::Function, dist::String)
+function lingape(
+    contexts::Array,
+    true_theta::Array,
+    delta::Real,
+    rate::Function,
+    dist::String,
+    sigma::Real = 1,
+    kappa::Real = 1,
+    epsilon::Real = 0.0,
+)
     # Initialization
     condition = true
     num_contexts = length(contexts)
@@ -10,60 +19,69 @@ function lingape(contexts::Array, theta::Array, delta::Real, rate::Function, dis
     # Initialize the prior
     lambda = sigma^2 / kappa^2
     design_inverse = Matrix{Float64}(1 / lambda * I, dim, dim)
-    square_root_inverse = Matrix{Float64}(kappa / sigma * I, dim, dim)
+    #square_root_inverse = Matrix{Float64}(kappa / sigma * I, dim, dim)
     z_t = vec(zeros(1, dim))
     rls = vec(zeros(1, dim))
     var = Matrix{Float64}(kappa^2 * I, dim, dim)
 
     # Play each arm once
     for c = 1:num_contexts
-        new_reward = compute_observation(contexts[c], theta, sigma)
+        t += 1
+        new_reward = compute_observation(contexts[c], true_theta, sigma)
         rewards[c] += new_reward
         num_pulls[c] = 1
 
         design_inverse = update_design_inverse(design_inverse, contexts[c])
-        square_root_inverse = update_square_root(square_root_inverse, contexts[c])
+        #square_root_inverse = update_square_root(square_root_inverse, contexts[c])
         z_t += new_reward * contexts[c]
         rls = design_inverse * z_t
         var = sigma^2 * design_inverse
     end
 
-    t = K
-    Best = 1
+    best = 1
     while (condition)
-        Mu = S ./ N
+        empirical_means = [dot(contexts[c], rls) for c = 1:num_contexts]
         # Empirical best arm
-        Best = randmax(Mu)
-        # Find the challenger
-        UCB = zeros(1, K)
-        LCB = zeros(1, K)
-        for a = 1:K
-            UCB[a] = dup(Mu[a], rate(t, delta) / N[a], dist)
-            LCB[a] = dlow(Mu[a], rate(t, delta) / N[a], dist)
-        end
-        B = zeros(1, K)
-        for a = 1:K
-            Index = collect(1:K)
-            deleteat!(Index, a)
-            B[a] = maximum(UCB[Index]) - LCB[a]
-        end
-        Value = minimum(B)
-        Best = argmin(B)[2]
-        UCB[Best] = 0
-        Challenger = argmax(UCB)
-        # choose which arm to draw
-        t = t + 1
-        I = (N[Best] < N[Challenger]) ? Best : Challenger
-        S[I] += sample_arm(mu[I], dist)
-        N[I] += 1
-        # check stopping condition
-        condition = (Value > 0)
-        if (t > 1000000)
+        best = randmax(empirical_means)
+        # Compute the stopping statistic
+        index = collect(1:num_contexts)
+        deleteat!(index, best)
+        # Compute the minimum GLR
+        score = minimum([compute_transportation(contexts[best], contexts[i], rls, var) for i in 1:num_contexts if i != best])
+        if (score > rate(t, delta))
+            # Stop
             condition = false
-            Best = 0
-            N = zeros(1, K)
+        elseif (t > 1e7)
+            condition = false
+            best = 0
+            println(num_pulls)
+            println(rewards)
+            num_pulls = zeros(1, num_contexts)
+        else
+            ambiguous = randmax([gaps(contexts[i], contexts[best], rls) +
+                                 confidence(contexts[i], contexts[best], design_inverse) for i = 1:num_contexts])
+            ucb = maximum([gaps(contexts[i], contexts[best], rls) +
+                           confidence(contexts[i], contexts[best], design_inverse) for i = 1:num_contexts])
+            if ucb <= epsilon
+                recommendation = best
+                return recommendation, num_pulls
+            end
+            new_sample = randmin([confidence(
+                contexts[best],
+                contexts[ambiguous],
+                update_design_inverse(design_inverse, contexts[i]),
+            ) for i = 1:num_contexts])
+            # Play the selected arm
+            t += 1
+            new_reward = compute_observation(contexts[new_sample], true_theta, sigma)
+            rewards[new_sample] += new_reward
+            num_pulls[new_sample] += 1
+
+            # Update the posterior
+            design_inverse = update_design_inverse(design_inverse, contexts[new_sample])
+            z_t += new_reward * contexts[new_sample]
+            rls = design_inverse * z_t
+            var = sigma^2 * design_inverse
         end
     end
-    recommendation = Best
-    return (recommendation, N)
 end
